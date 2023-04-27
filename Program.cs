18bd9707724914d87bed2854d370804278761e68
andrew <foki.andrew@gmail.com>
2023-04-27 12:00:37 +0300

    final version

--- Program.cs
+++ Program.cs
@@ -1,58 +1,23 @@
-﻿internal class Program
+﻿using static System.Console;
+internal class Program
 {
   private static void Main(string[] args)
   {
-    Console.WriteLine("-----------------------------------------------------------");
-
-    string header;
-    header = String.Format("{0}    {1}          {2}       {3}", "Type", "Byte(s) of memory", "Min", "Max");
-    Console.WriteLine(header);
-
-    Console.WriteLine("-----------------------------------------------------------");
-
-    string one;
-    one = String.Format("sbyte   {0}                         {1}       {2}",sizeof(sbyte), sbyte.MinValue, sbyte.MaxValue);
-    Console.WriteLine(one);
-
-    string two;
-    two = String.Format("byte    {0}                            {1}       {2}",sizeof(byte), byte.MinValue, byte.MaxValue);
-    Console.WriteLine(two);
-
-    string three;
-    three = String.Format("short   {0}                       {1}     {2}",sizeof(short), short.MinValue, short.MaxValue);
-    Console.WriteLine(three);
-
-    string four;
-    four = String.Format("ushort  {0}                           {1}     {2}",sizeof(ushort), ushort.MinValue,  ushort.MaxValue);
-    Console.WriteLine(four);
-
-    string five;
-    five = String.Format("int     {0}                   {1} {2}",sizeof(int), int.MinValue,  int.MaxValue);
-    Console.WriteLine(five);
-
-    string six;
-    six = String.Format("uint    {0}                   {1}    {2}",sizeof(uint), uint.MinValue,  uint.MaxValue);
-    Console.WriteLine(six);
-
-    string seven;
-    seven = String.Format("long    {0}                   {1}    {2}",sizeof(long), long.MinValue,  long.MaxValue);
-    Console.WriteLine(seven);
-
-    string eight;
-    eight = String.Format("ulong   {0}                   {1}    {2}",sizeof(ulong), ulong.MinValue,  ulong.MaxValue);
-    Console.WriteLine(eight);  
-    
-    string nine;
-    nine = String.Format("float   {0}                   {1}    {2}",sizeof(float), float.MinValue,  float.MaxValue);
-    Console.WriteLine(nine);  
-
-    string ten;
-    ten = String.Format("double  {0}                   {1}    {2}",sizeof(double), double.MinValue,  double.MaxValue);
-    Console.WriteLine(ten);    
-
-    string eleven;
-    eleven = String.Format("decimal {0}                   {1}    {2}",sizeof(decimal), decimal.MinValue,  decimal.MaxValue);
-    Console.WriteLine(eleven);   
+    WriteLine("--------------------------------------------------------------------------");
+            WriteLine("Type    Byte(s) of memory               Min                            Max");
+            WriteLine("--------------------------------------------------------------------------");
+            WriteLine($"sbyte   {sizeof(sbyte),-4} {sbyte.MinValue,30} {sbyte.MaxValue,30}");
+            WriteLine($"byte    {sizeof(byte),-4} {byte.MinValue,30} {byte.MaxValue,30}");
+            WriteLine($"short   {sizeof(short),-4} {short.MinValue,30} {short.MaxValue,30}");
+            WriteLine($"ushort  {sizeof(ushort),-4} {ushort.MinValue,30} {ushort.MaxValue,30}");
+            WriteLine($"int     {sizeof(int),-4} {int.MinValue,30} {int.MaxValue,30}");
+            WriteLine($"uint    {sizeof(uint),-4} {uint.MinValue,30} {uint.MaxValue,30}");
+            WriteLine($"long    {sizeof(long),-4} {long.MinValue,30} {long.MaxValue,30}");
+            WriteLine($"ulong   {sizeof(ulong),-4} {ulong.MinValue,30} {ulong.MaxValue,30}");
+            WriteLine($"float   {sizeof(float),-4} {float.MinValue,30} {float.MaxValue,30}");
+            WriteLine($"double  {sizeof(double),-4} {double.MinValue,30} {double.MaxValue,30}");
+            WriteLine($"decimal {sizeof(decimal),-4} {decimal.MinValue,30} {decimal.MaxValue,30}");
+            WriteLine("--------------------------------------------------------------------------");
 
   }
 }
