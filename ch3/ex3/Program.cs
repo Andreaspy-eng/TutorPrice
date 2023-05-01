51ac97dfee5325b7e700e897f8bdae50e82a7ea4
andrew <foki.andrew@gmail.com>
2023-05-01 15:18:21 +0300

    ex3

---
+++ ch3/ex3/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+internal partial class Program
+{
+  private static void Main(string[] args)
+  {
+    for(int i =0; i<=100; i++)
+    {
+      if (i % 15 == 0 )
+      {
+        Write("FizzBuzz, ");
+      }
+      else if (i % 5 == 0)
+      {
+        Write("Buzz, ");
+      }
+      else if (i %3 ==0)
+      {
+        Write("Fizz, ");
+      }
+      else
+      {
+        Write($"{i}, ");
+      }
+    }
+
+  }
+}
