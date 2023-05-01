78bb237bdcd4aff444c3a24838a64c6bf1f6fb7c
andrew <foki.andrew@gmail.com>
2023-05-01 15:50:09 +0300

    ex4

---
+++ ch3/ex4/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    try
+    {
+      Write("Enter a number between 0 and 255: ");
+      int first = Int32.Parse(ReadLine());
+      Write($"Enter another number between 0 and 255: ");
+      int second = Int32.Parse(ReadLine());
+      WriteLine($"{first} divided by {second} is {first / second}");
+    }
+    catch(Exception ex)
+    {
+      WriteLine($"{ex.GetType().Name}: {ex.Message}");
+    }
+    
+  }
+}
